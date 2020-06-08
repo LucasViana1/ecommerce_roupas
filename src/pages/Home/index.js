f634cfbb31f8bf6ed694f18990361e723b8073d3
Lucas Viana <lucasviana112@gmail.com>
2020-06-08 14:59:07 -0300

    pagina principal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './styles.css';
 
 import ProductSale from '../../components/ProductSale';
+import api from '../../services/api';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const { data } = await api.get();
+      console.log('response');
+      console.log(data);
+      const setializedData = data.map((item, index) => {
+        return {
+          id: index,
+          ...item,
+        };
+      });
+      console.log('setializedData');
+      console.log(setializedData);
+      // salva no state local e na store
+      setProducts(setializedData);
+    };
+    loadProducts();
+  }, []);
+
   return (
     <main className="home">
-      <ProductSale />
-      <ProductSale />
-      <ProductSale />
-      <ProductSale />
-      <ProductSale />
+      {products &&
+        products.map((product) => (
+          <ProductSale key={product.code_color} product={product} />
+        ))}
     </main>
   );
 };

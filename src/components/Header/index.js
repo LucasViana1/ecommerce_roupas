c5a31a0a640b9867557ccbc5130305c940ee5189
Lucas Viana <lucasviana112@gmail.com>
2020-06-08 15:00:22 -0300

    cabeçalho fixo, navagação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './styles.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { openModalCart } from '../../store/modules/cart/action';
 
 import logo from '../../assets/logo.png';
+import ModalCart from '../ModalCart';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const { openCart } = useSelector((state) => state.cart);
+
   return (
     <header className="header">
       <section className="header__logo">
         <img src={logo} alt="Logo" />
       </section>
       <section className="header__icons">
-        <div className="header__search">
+        <button type="button" className="header__search">
           <i className="fas fa-search" />
-        </div>
-        <div className="header__cart">
+        </button>
+        <button
+          type="button"
+          className="header__cart"
+          onClick={() => dispatch(openModalCart(true))}
+        >
           <i className="fas fa-cart-arrow-down" />
-        </div>
+        </button>
       </section>
+      {openCart ? <ModalCart /> : null}
     </header>
   );
 };

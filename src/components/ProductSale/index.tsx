9b3088fa6a5345b4b0edebd8f6121fef70449a08
Lucas Viana <lucasviana112@gmail.com>
2020-06-09 19:39:04 -0300

    componente produtos listados na pagina principal

---
+++ src/components/ProductSale/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+interface ProductSaleProps {
+  product: {
+    id: number;
+    name: string;
+    image: string;
+    actual_price: string;
+    regular_price: string;
+    discount_percentage: string;
+  };
+}
+
+const ProductSale: React.FC<ProductSaleProps> = ({ product }) => {
+  const {
+    id,
+    name,
+    image,
+    actual_price,
+    regular_price,
+    discount_percentage,
+  } = product;
+
+  return (
+    <article className="productsale">
+      <Link to={`/product/${id}`}>
+        {console.log('props')}
+        {console.log(product)}
+        <section className="productsale__container">
+          {/* <div> */}
+          {discount_percentage ? (
+            <div className="productsale_discount">
+              <span>-{discount_percentage}</span>
+            </div>
+          ) : null}
+          <img
+            src={
+              image ||
+              'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indisponível'
+            }
+            alt={name}
+          />
+          {/* </div> */}
+        </section>
+        <section className="productsale__labels">
+          <strong>{name}</strong>
+          <span className="productsale__price">
+            <div className={discount_percentage ? 'productsale__priceold' : ''}>
+              {regular_price}
+            </div>
+            {discount_percentage ? actual_price : ''}
+          </span>
+        </section>
+      </Link>
+    </article>
+  );
+};
+
+export default ProductSale;

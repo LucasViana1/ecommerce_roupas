fa2f52a11a5f5e187d52f22f55b6b126ca4e9a71
Lucas Viana <lucasviana112@gmail.com>
2020-06-09 12:04:57 -0300

    ajuste link de navegação

--- src/components/ProductSale/index.js
+++ src/components/ProductSale/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const ProductSale = ({ product }) => {
@@ -14,33 +14,35 @@ const ProductSale = ({ product }) => {
 
   return (
     <article className="productsale">
-      {console.log('props')}
-      {console.log(product)}
-      <section className="productsale__container">
-        {/* <div> */}
-        {discount_percentage ? (
-          <div className="productsale_discount">
-            <span>-{discount_percentage}</span>
-          </div>
-        ) : null}
-        <img
-          src={
-            image ||
-            `https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indisponível`
-          }
-          alt={name}
-        />
-        {/* </div> */}
-      </section>
-      <section className="productsale__labels">
-        <strong>{name}</strong>
-        <span className="productsale__price">
-          <div className={discount_percentage ? 'productsale__priceold' : ''}>
-            {regular_price}
-          </div>
-          {discount_percentage ? actual_price : ''}
-        </span>
-      </section>
+      <Link to={`/product/${id}`}>
+        {console.log('props')}
+        {console.log(product)}
+        <section className="productsale__container">
+          {/* <div> */}
+          {discount_percentage ? (
+            <div className="productsale_discount">
+              <span>-{discount_percentage}</span>
+            </div>
+          ) : null}
+          <img
+            src={
+              image ||
+              `https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indisponível`
+            }
+            alt={name}
+          />
+          {/* </div> */}
+        </section>
+        <section className="productsale__labels">
+          <strong>{name}</strong>
+          <span className="productsale__price">
+            <div className={discount_percentage ? 'productsale__priceold' : ''}>
+              {regular_price}
+            </div>
+            {discount_percentage ? actual_price : ''}
+          </span>
+        </section>
+      </Link>
     </article>
   );
 };

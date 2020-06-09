8064ffb7ae4ba54f0eec7e5d6143e7e7c9f24035
Lucas Viana <lucasviana112@gmail.com>
2020-06-09 20:37:58 -0300

    troca icones carrinho e pesquisa

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './styles.css';
+import { FiSearch, FiShoppingCart } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { openModalCart } from '../../store/modules/cart/action';
+import { openModalSearch } from '../../store/modules/search/action';
 
 import ModalCart from '../ModalCart';
+import ModalSearch from '../ModalSearch';
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
 
   const { openCart } = useSelector((state: any) => state.cart);
+  const { openSearch } = useSelector((state: any) => state.search);
 
   return (
     <header className="header">
@@ -20,18 +24,25 @@ const Header: React.FC = () => {
         </section>
       </Link>
       <section className="header__icons">
-        <button type="button" className="header__search">
-          <i className="fas fa-search" />
+        <button
+          type="button"
+          className="header__search"
+          onClick={() => dispatch(openModalSearch(true))}
+        >
+          {/* <i className="fas fa-search" /> */}
+          <FiSearch className="header__search--icon" />
         </button>
         <button
           type="button"
           className="header__cart"
           onClick={() => dispatch(openModalCart(true))}
         >
-          <i className="fas fa-cart-arrow-down" />
+          {/* <i className="fas fa-cart-arrow-down" /> */}
+          <FiShoppingCart className="header__cart--icon" />
         </button>
       </section>
       {openCart ? <ModalCart /> : null}
+      {openSearch ? <ModalSearch /> : null}
     </header>
   );
 };

809aab2816da00bf5749548f03fcfa8f4d748385
Lucas Viana <lucasviana112@gmail.com>
2020-06-09 12:01:21 -0300

    declaração inicial componente rodape

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import './styles.css';
+// // import { Link } from 'react-router-dom';
+
+// const Header = () => {
+
+//   return (
+//     <footer className="footer">
+
+//     </footer>
+//   );
+// };
+
+// export default Header;

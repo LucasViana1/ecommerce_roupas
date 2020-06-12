1d1caa9d5c4414be647d998dd9fefc0fddabe350
Lucas Viana <lucasviana112@gmail.com>
2020-06-11 22:52:18 -0300

    componente de exibição do produto no modal de pesquisa

---
+++ src/components/ProductSearch/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+
+const ProductSearch: React.FC = () => (
+  <>
+    <article className="productsearch">
+      <section className="productsearch__avatar">
+        <img
+          src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912"
+          alt="vestido"
+        />
+      </section>
+      <section className="productsearch__details">
+        <strong>VESTIDO TRANSPASSE BOW</strong>
+        <div>
+          <span>R$ 200,00</span>
+          <span>4x R$ 50,00</span>
+        </div>
+      </section>
+      {/* <section className="productcart__buttons">
+        <div className="productcart__quantify">
+          <button type="button">+</button>
+          <input type="text" name="qtd" id="qtd" readOnly value={1} />
+          <button type="button">-</button>
+        </div>
+        <button type="button">Remover</button>
+      </section> */}
+    </article>
+    <hr style={{ width: '94%' }} />
+  </>
+);
+
+export default ProductSearch;

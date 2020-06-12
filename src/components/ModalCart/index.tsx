fcdf137bd5a384bdff968e96ddc152615ec55276
Lucas Viana <lucasviana112@gmail.com>
2020-06-11 23:00:59 -0300

    icones e layout modal de carrinho

--- src/components/ModalCart/index.tsx
+++ src/components/ModalCart/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import "./styles.css";
-import { useDispatch } from "react-redux";
-import { openModalCart } from "../../store/modules/cart/action";
+import React from 'react';
+import './styles.css';
+import { useDispatch } from 'react-redux';
+import { FiXSquare } from 'react-icons/fi';
+import { openModalCart } from '../../store/modules/cart/action';
 
-import ProductCart from "../ProductCart";
+import ProductCart from '../ProductCart';
 
 const ModalCart = () => {
   const dispatch = useDispatch();
@@ -12,11 +13,13 @@ const ModalCart = () => {
     <article className="cart">
       <header className="cart__header">
         <button type="button" onClick={() => dispatch(openModalCart(false))}>
-          Fechar carrinho
+          <FiXSquare className="cart__icon--x" />
         </button>
-        <span>Sacola (3)</span>
+        <span>
+          <p>Sacola (3)</p>
+        </span>
       </header>
-      <section className="cart_products">
+      <section className="cart__products">
         {/* loop carrinho */}
         <ProductCart />
         <ProductCart />

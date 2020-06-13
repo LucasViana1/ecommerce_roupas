bd0f95f6585b04ae1b1e3a73dbd00b11d3d19c30
Lucas Viana <lucasviana112@gmail.com>
2020-06-13 16:43:16 -0300

    inserção novas dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import store from './store';
 
 import Routes from './routes';
 import Header from './components/Header';
+// import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
+      {/* <Router history={history}> */}
       <BrowserRouter>
         <Header />
         <Routes />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          // hideProgressBar={false}
+          // newestOnTop={false}
+          // closeOnClick
+          // rtl={false}
+          // pauseOnFocusLoss
+          // draggable
+          // pauseOnHover
+        />
       </BrowserRouter>
     </Provider>
   );
--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -1,44 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
+import { useDispatch } from 'react-redux';
 import { FiTrash2, FiPlusCircle, FiMinusCircle } from 'react-icons/fi';
+import { removeProductFromCart } from '../../store/modules/cart/action';
 
-const ProductCart = () => (
-  <>
-    <article className="productcart">
-      <section className="productcart__avatar">
-        <img
-          src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912"
-          alt="vestido"
-        />
-      </section>
-      <section className="productcart__details">
-        <strong>VESTIDO TRANSPASSE BOW</strong>
-        <div>
-          <span>R$ 200,00</span>
-          <span>4x R$ 50,00</span>
-        </div>
-      </section>
-      <section className="productcart__buttons">
-        <div className="productcart__quantify">
-          <button type="button">
-            <FiMinusCircle className="productcart__icons--minusplus" />
+interface ProductCartProps {
+  detail: {
+    id: number;
+    quantity: number;
+    actual_price: string;
+    discount_percentage: string;
+    image: string;
+    installments: string;
+    name: string;
+    regular_price: string;
+    selectedSize: string;
+  };
+  handleRemove: (remove: boolean) => void;
+  remove: boolean;
+}
+
+const ProductCart: React.FC<ProductCartProps> = ({
+  detail,
+  handleRemove,
+  remove,
+}) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const dispatch = useDispatch();
+
+  function handleQuantity(minus = false) {
+    if (minus) {
+      if (quantity === 1) setQuantity(quantity);
+      else setQuantity(quantity - 1);
+    } else {
+      setQuantity(quantity + 1);
+    }
+  }
+
+  function handleRemoveProduct() {
+    dispatch(removeProductFromCart(detail.id));
+    handleRemove(!remove);
+  }
+
+  return (
+    <>
+      <article className="productcart">
+        <section className="productcart__avatar">
+          <img src={detail.image} alt={detail.name} />
+        </section>
+        <section className="productcart__details">
+          <strong>{detail.name}</strong>
+          {console.log(detail)}
+          <div>
+            <span>{detail.actual_price}</span>
+            <span>{detail.installments}</span>
+          </div>
+        </section>
+        <section className="productcart__buttons">
+          <div className="productcart__quantify">
+            <button type="button" onClick={() => handleQuantity(true)}>
+              <FiMinusCircle className="productcart__icons--minusplus" />
+            </button>
+            <input type="text" name="qtd" id="qtd" readOnly value={quantity} />
+            <button type="button" onClick={() => handleQuantity()}>
+              <FiPlusCircle className="productcart__icons--minusplus" />
+            </button>
+          </div>
+          <button
+            type="button"
+            className="productcart__buttons--remove"
+            onClick={() => handleRemoveProduct()}
+          >
+            <span>
+              <FiTrash2 className="productcart__icons--remove" />
+            </span>
+            REMOVER
           </button>
-          <input type="text" name="qtd" id="qtd" readOnly value={1} />
-          <button type="button">
-            <FiPlusCircle className="productcart__icons--minusplus" />
-          </button>
-        </div>
-        <button type="button" className="productcart__buttons--remove">
-          <span>
-            <FiTrash2 className="productcart__icons--remove" />
-          </span>
-          REMOVER
-        </button>
-      </section>
-    </article>
-    <hr style={{ width: '94%' }} />
-  </>
-);
+        </section>
+      </article>
+      <hr style={{ width: '94%' }} />
+    </>
+  );
+};
 
 export default ProductCart;

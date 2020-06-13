6e2192ea67ba64812690db6aed95226d6383a4aa
Lucas Viana <lucasviana112@gmail.com>
2020-06-13 16:40:14 -0300

    exibição dinamiza de produtos no carrinho, com quantidade e funcionalidade de remoção de produto

--- src/components/ModalCart/index.tsx
+++ src/components/ModalCart/index.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FiXSquare } from 'react-icons/fi';
 import { openModalCart } from '../../store/modules/cart/action';
 
 import ProductCart from '../ProductCart';
 
 const ModalCart = () => {
+  const [productsWithQuantity, setProductsWithQuantity] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [remove, setRemove] = useState(false);
+
   const dispatch = useDispatch();
+
+  const { productList } = useSelector((state: any) => state.cart);
+
+  useEffect(() => {
+    const list: any = [];
+
+    for (let i = 0; i < productList.length; i += 1) {
+      list[i] = {
+        ...productList[i],
+        quantity: 1,
+      };
+    }
+
+    setProductsWithQuantity(list);
+    setAmount(list.length);
+
+    // list.sort(function compare(a: any, b: any) {
+    //   if (a.id < b.id) return -1;
+    //   if (a.id > b.id) return 1;
+    //   return 0;
+    // });
+    // console.log('list cart...');
+    // console.log(list);
+
+    // for (let i = 0; i < list.length; i += 1) {
+    //   const repeatedProduct = list.findIndex((p: any) => p.id === list[i].id);
+    //   if (repeatedProduct >= 0) {
+    //     list[i].quantity += 1;
+    //     list.splice(repeatedProduct, 1);
+    //   }
+    // }
+  }, [remove]);
 
   return (
     <article className="cart">
@@ -16,15 +52,30 @@ const ModalCart = () => {
           <FiXSquare className="cart__icon--x" />
         </button>
         <span>
-          <p>Sacola (3)</p>
+          <p>Sacola ({amount})</p>
         </span>
       </header>
       <section className="cart__products">
-        {/* loop carrinho */}
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
+        {/* {console.log(productsWithQuantity)} */}
+        {productsWithQuantity &&
+          productsWithQuantity.map((product: any) => (
+            <ProductCart
+              key={product.id}
+              detail={product}
+              handleRemove={setRemove}
+              remove={remove}
+            />
+          ))}
       </section>
+      <div className="cart__products--submit">
+        {productsWithQuantity.length === 0 ? (
+          <div>Nenhum produto no carrinho</div>
+        ) : (
+          <button type="button" onClick={() => {}}>
+            Finalizar pedido
+          </button>
+        )}
+      </div>
     </article>
   );
 };
